d6bd983597c07c491a12693df14f6d96e95467a4
Kamil Monicz <kamil@monicz.pl>
2023-11-19 08:56:25 +0100

    _get_path documentation and boundary check

--- lib/file_cache.py
+++ lib/file_cache.py
@@ -5,27 +5,49 @@ from anyio import Path
 
 from config import FILE_CACHE_DIR
 
-_FILE_WHITELIST_CHARS = set(string.ascii_letters + string.digits + '._-')
+_FILE_WHITELIST_CHARS = set(string.ascii_letters + string.digits + '._- ')
 
 
 class FileCache:
     _base_dir: Path
+    _base_dir_resolved: Path | None
 
     def __init__(self, context: str, *, cache_dir: Path = FILE_CACHE_DIR):
         self._base_dir = cache_dir / context
+        self._base_dir_resolved = None
 
     async def _get_path(self, key: str) -> Path:
-        if not all(c in _FILE_WHITELIST_CHARS for c in key):
-            raise ValueError('key contains invalid characters')
+        """
+        Get the path to a file in the file cache by key string.
+
+        >>> await FileCache('context')._get_path('file_key')
+        Path('.../context/f/il/file_key')
+        """
 
         if len(key) < 3:
             raise ValueError('key must be at least 3 characters long')
 
+        if not all(c in _FILE_WHITELIST_CHARS for c in key):
+            raise ValueError('key contains invalid characters')
+
         d1 = key[:1]
         d2 = key[1:3]
+
+        if '.' in d1 or '.' in d2:
+            raise ValueError('key cannot start with a dot')
+
         dir_path = self._base_dir / d1 / d2
+        full_path = dir_path / key
+        full_path = await full_path.resolve()
+
+        if self._base_dir_resolved is None:
+            self._base_dir_resolved = await self._base_dir.resolve()
+
+        if not full_path.is_relative_to(self._base_dir_resolved):
+            raise ValueError('key is invalid')
+
         await dir_path.mkdir(parents=True, exist_ok=True)
-        return dir_path / key
+        return full_path
 
     async def get(self, key: str) -> bytes | None:
         """
