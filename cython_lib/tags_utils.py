406ce0d8327b6906704586ab4b6b6a37b24572b1
Kamil Monicz <kamil@monicz.dev>
2023-12-18 21:45:48 +0100

    Style tags and handle color preview

---
+++ cython_lib/tags_utils.py
@@ -0,0 +1,203 @@
+import cython
+
+if cython.compiled:
+    print(f'{__name__}: 🐇 compiled')
+else:
+    print(f'{__name__}: 🐌 not compiled')
+
+
+def color_preview(key: str, value: str) -> str | None:
+    """
+    Return a color preview of a string.
+
+    Returns None if the string is not a valid color.
+
+    >>> color_preview('colour', '#ff0000')
+    '#ff0000'
+
+    >>> color_preview('colour', 'aliceblue')
+    'aliceblue'
+
+    >>> color_preview('colour', 'invalid')
+    None
+    """
+
+    # small optimization, skip empty values
+    if not value:
+        return None
+
+    # match keys like r':?colour$' without regex/backtracking
+    if not key.endswith('colour'):
+        return None
+    if len(key) > 6 and key[-7] != ':':
+        return None
+
+    # match hex colors efficiently
+    if value[0] == '#':
+        if len(value) not in (4, 7):
+            return None
+        for c in value[1:]:
+            if not (('0' <= c <= '9') or ('A' <= c <= 'F') or ('a' <= c <= 'f')):
+                return None
+
+        return value
+
+    # match W3C colors
+    if value.lower() in _W3C_COLORS:
+        return value
+
+    return None
+
+
+# source: https://www.w3.org/TR/css-color-3/#svg-color
+_W3C_COLORS = frozenset(
+    (
+        'aliceblue',
+        'antiquewhite',
+        'aqua',
+        'aquamarine',
+        'azure',
+        'beige',
+        'bisque',
+        'black',
+        'blanchedalmond',
+        'blue',
+        'blueviolet',
+        'brown',
+        'burlywood',
+        'cadetblue',
+        'chartreuse',
+        'chocolate',
+        'coral',
+        'cornflowerblue',
+        'cornsilk',
+        'crimson',
+        'cyan',
+        'darkblue',
+        'darkcyan',
+        'darkgoldenrod',
+        'darkgray',
+        'darkgrey',
+        'darkgreen',
+        'darkkhaki',
+        'darkmagenta',
+        'darkolivegreen',
+        'darkorange',
+        'darkorchid',
+        'darkred',
+        'darksalmon',
+        'darkseagreen',
+        'darkslateblue',
+        'darkslategray',
+        'darkslategrey',
+        'darkturquoise',
+        'darkviolet',
+        'deeppink',
+        'deepskyblue',
+        'dimgray',
+        'dimgrey',
+        'dodgerblue',
+        'firebrick',
+        'floralwhite',
+        'forestgreen',
+        'fuchsia',
+        'gainsboro',
+        'ghostwhite',
+        'gold',
+        'goldenrod',
+        'gray',
+        'grey',
+        'green',
+        'greenyellow',
+        'honeydew',
+        'hotpink',
+        'indianred',
+        'indigo',
+        'ivory',
+        'khaki',
+        'lavender',
+        'lavenderblush',
+        'lawngreen',
+        'lemonchiffon',
+        'lightblue',
+        'lightcoral',
+        'lightcyan',
+        'lightgoldenrodyellow',
+        'lightgray',
+        'lightgrey',
+        'lightgreen',
+        'lightpink',
+        'lightsalmon',
+        'lightseagreen',
+        'lightskyblue',
+        'lightslategray',
+        'lightslategrey',
+        'lightsteelblue',
+        'lightyellow',
+        'lime',
+        'limegreen',
+        'linen',
+        'magenta',
+        'maroon',
+        'mediumaquamarine',
+        'mediumblue',
+        'mediumorchid',
+        'mediumpurple',
+        'mediumseagreen',
+        'mediumslateblue',
+        'mediumspringgreen',
+        'mediumturquoise',
+        'mediumvioletred',
+        'midnightblue',
+        'mintcream',
+        'mistyrose',
+        'moccasin',
+        'navajowhite',
+        'navy',
+        'oldlace',
+        'olive',
+        'olivedrab',
+        'orange',
+        'orangered',
+        'orchid',
+        'palegoldenrod',
+        'palegreen',
+        'paleturquoise',
+        'palevioletred',
+        'papayawhip',
+        'peachpuff',
+        'peru',
+        'pink',
+        'plum',
+        'powderblue',
+        'purple',
+        'red',
+        'rosybrown',
+        'royalblue',
+        'saddlebrown',
+        'salmon',
+        'sandybrown',
+        'seagreen',
+        'seashell',
+        'sienna',
+        'silver',
+        'skyblue',
+        'slateblue',
+        'slategray',
+        'slategrey',
+        'snow',
+        'springgreen',
+        'steelblue',
+        'tan',
+        'teal',
+        'thistle',
+        'tomato',
+        'turquoise',
+        'violet',
+        'wheat',
+        'white',
+        'whitesmoke',
+        'yellow',
+        'yellowgreen',
+    )
+)
