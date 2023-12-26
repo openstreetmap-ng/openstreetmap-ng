a76adffe72de920e855ed611db15afb2bb6e6d5f
Kamil Monicz <kamil@monicz.dev>
2023-12-26 01:16:07 +0100

    Migrate matomo script

---
+++ static/js/matomo.js
@@ -0,0 +1,20 @@
+window._paq = window._paq || []
+const _paq = window._paq
+
+// tracker methods like "setCustomDimension" should be called before "trackPageView"
+_paq.push(["setDocumentTitle", `${location.hostname}/${document.title}`])
+_paq.push(["setDoNotTrack", true])
+_paq.push(["trackPageView"])
+_paq.push(["enableLinkTracking"])
+const url = "https://matomo.monicz.dev/"
+_paq.push(["setTrackerUrl", `${url}matomo.php`])
+_paq.push(["setSiteId", "4"])
+
+const newScript = document.createElement("script")
+newScript.async = true
+newScript.src = `${url}matomo.js`
+
+const existingScript = document.getElementsByTagName("script")[0]
+existingScript.parentNode.insertBefore(newScript, existingScript)
+
+// TODO: matomogoal meta[name=matomo-goal] trackGoal
