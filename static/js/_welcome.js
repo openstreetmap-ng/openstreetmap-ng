03a9fe9897dfabea05cfb198cd261f77f68b9df7
Kamil Monicz <kamil@monicz.dev>
2023-12-26 23:50:32 +0100

    Prefer lon, lat, zoom ordering for consistency

--- static/js/_welcome.js
+++ static/js/_welcome.js
@@ -8,13 +8,15 @@ if (noteIcon) {
     // Support default location setting via URL parameters
     let locationProvided = false
     const params = qsParse(window.location.search.substring(1))
-    if (params.lat && params.lon) {
+    if (params.lon && params.lat) {
+        params.lon = parseFloat(params.lon)
+        params.lat = parseFloat(params.lat)
         // Zoom is optional, default to 17
         params.zoom = parseInt(params.zoom || 17, 10)
-        params.lat = parseFloat(params.lat)
-        params.lon = parseFloat(params.lon)
 
-        if (isZoom(params.zoom) && isLatitude(params.lat) && isLongitude(params.lon)) locationProvided = true
+        if (isLongitude(params.lon) && isLatitude(params.lat) && isZoom(params.zoom)) {
+            locationProvided = true
+        }
     }
 
     // Assign position only if it's valid
@@ -32,9 +34,9 @@ if (noteIcon) {
         // If location was not provided, request navigator.geolocation
         // On geolocation success, redirect to /edit with the returned coordinates
         const onGeolocationSuccess = (position) => {
-            params.zoom = 17
-            params.lat = position.coords.latitude
             params.lon = position.coords.longitude
+            params.lat = position.coords.latitude
+            params.zoom = 17
 
             if (Object.keys(startParams).length > 0) startHref += `?${qsStringify(startParams)}`
             startHref += formatHash(params)
