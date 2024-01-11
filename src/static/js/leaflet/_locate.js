f2d2b00a503f042af85955c0056ad6f8bb7e1e48
Kamil Monicz <kamil@monicz.dev>
2024-01-11 20:23:33 +0100

    Migrate locate control script

---
+++ src/static/js/leaflet/_locate.js
@@ -0,0 +1,24 @@
+import * as L from "leaflet"
+import "leaflet.locatecontrol"
+
+// TODO: import (S)CSS?
+
+export const getLocateControl = (options) => {
+    const control = L.control.locate(
+        Object.assign(
+            {
+                icon: "icon geolocate",
+                iconLoading: "icon geolocate",
+                strings: {
+                    title: I18n.t("javascripts.map.locate.title"),
+                    popup: ({ distance, unit }) => {
+                        return I18n.t(`javascripts.map.locate.${unit}Popup`, { count: distance })
+                    },
+                },
+            },
+            options,
+        ),
+    )
+
+    return control
+}
