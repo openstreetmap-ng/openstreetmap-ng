041d71ad1c4037a62767022cde0f4d832a7069a3
Kamil Monicz <kamil@monicz.dev>
2024-01-18 09:35:48 +0100

    Optimize naturalsize cython and add docs

--- app/libc/naturalsize.py
+++ app/libc/naturalsize.py
@@ -1,17 +1,21 @@
 import cython
 
-_suffixes = (' KiB', ' MiB', ' GiB', ' TiB', ' PiB', ' EiB', ' ZiB', ' YiB')
 
+def naturalsize(value: float) -> str:
+    """
+    Format a number of byteslike a human readable filesize (eg. 10 KiB, 2.3 MiB, 1.2 GiB, etc).
 
-def naturalsize(value: float | str) -> str:
-    """ """
+    >>> naturalsize(10000)
+    '9.8 KiB'
+    """
+
     base: cython.int = 1024
     bytes_: cython.double = float(value)
 
     if bytes_ < base:
         return f'{bytes_:.0f} B'
 
-    for s in _suffixes:
+    for s in (' KiB', ' MiB', ' GiB', ' TiB', ' PiB', ' EiB', ' ZiB', ' YiB'):
         bytes_ /= base
 
         if bytes_ < base:
