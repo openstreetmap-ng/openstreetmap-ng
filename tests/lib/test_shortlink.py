6f05e0e59a778aa746bc561d3e99c2073141fc3a
Kamil Monicz <kamil@monicz.dev>
2024-01-21 04:26:29 +0100

    Update test_shortlink.py

--- tests/lib/test_shortlink.py
+++ tests/lib/test_shortlink.py
@@ -1,4 +1,4 @@
-from math import isclose
+from math import isclose, sqrt
 
 import pytest
 
@@ -11,14 +11,19 @@ from app.lib.shortlink import shortlink_decode, shortlink_encode
         (0, 0, 5),
         (156, 45, 17),
         (1.23456789, 2.34567891, 20),
+        (-1.23456789, -2.34567891, 20),
+        (119.99999999, 39.99999999, 21),
+        (15.545454, 45.454545, 13),
     ],
 )
 def test_encode_decode(input):
     encoded = shortlink_encode(*input)
     decoded = shortlink_decode(encoded)
+    assert input[2] == decoded[2]  # zoom must be equal
 
-    for a, b in zip(input, decoded, strict=True):
-        assert isclose(a, b, abs_tol=0.01)
+    distance = sqrt((input[0] - decoded[0]) ** 2 + (input[1] - decoded[1]) ** 2)
+    max_distance = 360 / (2 ** (input[2] + 8)) * 0.5 * sqrt(5)
+    assert max_distance > distance
 
 
 @pytest.mark.parametrize(
@@ -33,3 +38,20 @@ def test_decode(input, output):
 
     for a, b in zip(output, decoded, strict=True):
         assert isclose(a, b, abs_tol=0.01)
+
+
+@pytest.mark.parametrize(
+    ('new', 'old'),
+    [
+        ('~v2juONc--', '@v2juONc--'),
+        ('as3I3GpG~-', 'as3I3GpG@-'),
+        ('D~hV--', 'D@hV--'),
+        ('CO0O~m8--', 'CO0O@m8--'),
+    ],
+)
+def test_deprecated_at_sign(new, old):
+    decoded1 = shortlink_decode(new)
+    decoded2 = shortlink_decode(old)
+
+    for a, b in zip(decoded1, decoded2, strict=True):
+        assert isclose(a, b)
