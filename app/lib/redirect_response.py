a35e0ba515db0e01e0104dbdeeafc5b9f649c76e
Kamil Monicz <kamil@monicz.dev>
2024-03-06 08:58:48 +0100

    Login and signup pages

---
+++ app/lib/redirect_response.py
@@ -0,0 +1,71 @@
+import logging
+from urllib.parse import unquote_plus, urlsplit
+
+import cython
+from starlette import status
+from starlette.responses import RedirectResponse
+
+from app.middlewares.request_context_middleware import get_request
+
+
+@cython.cfunc
+def _process_referrer(referrer: str) -> str | None:
+    """
+    Process the referrer value.
+
+    Returns None if the referrer is missing or is in a different domain.
+    """
+
+    if not referrer:
+        return None
+
+    # return relative values as-is
+    if referrer[0] == '/':
+        return referrer
+
+    # otherwise, validate the referrer hostname
+    parts = urlsplit(referrer, allow_fragments=False)
+
+    referrer_hostname = parts.hostname
+    request_hostname = get_request().url.hostname
+
+    if referrer_hostname != request_hostname:
+        logging.debug('Referrer hostname mismatch (%r != %r)', referrer_hostname, request_hostname)
+        return None
+
+    return referrer
+
+
+@cython.cfunc
+def _redirect_url() -> str:
+    """
+    Get the redirect URL from the request referrer.
+
+    If the referrer is missing or is in a different domain, return '/'.
+    """
+
+    request = get_request()
+
+    # referrer as a query parameter
+    referrer = request.query_params.get('referer')
+    if referrer is not None:
+        processed = _process_referrer(unquote_plus(referrer))
+        if processed is not None:
+            return processed
+
+    # referrer as a header
+    referrer = request.headers.get('Referer')
+    if referrer is not None:
+        processed = _process_referrer(referrer)
+        if processed is not None:
+            return processed
+
+    return '/'
+
+
+def redirect_response() -> RedirectResponse:
+    """
+    Get a redirect response, respecting the referrer header.
+    """
+
+    return RedirectResponse(_redirect_url(), status.HTTP_303_SEE_OTHER)
