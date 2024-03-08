065ad3bc30ef89f22cb42a119a2ae22eca4bdfb4
Kamil Monicz <kamil@monicz.dev>
2024-03-08 08:21:12 +0100

    Simplify residence terms detection

--- app/static/js/_user-terms.js
+++ app/static/js/_user-terms.js
@@ -1,15 +1,14 @@
 const userTermsBody = document.querySelector("body.user-terms-body")
 if (userTermsBody) {
     const residenceInputs = userTermsBody.querySelectorAll("input[name=residence]")
-    const legalDocuments = userTermsBody.querySelectorAll("[data-legal-document]")
+    const legalDocuments = userTermsBody.querySelectorAll(".legal-document[data-residence]")
 
     const onResidenceChange = (e) => {
         const newResidence = e.target.value
         console.debug("onResidenceChange", e.target.value)
 
         for (const legalDocument of legalDocuments) {
-            const residence = legalDocument.dataset.legalDocument
-            legalDocument.classList.toggle("d-none", residence !== newResidence)
+            legalDocument.classList.toggle("d-none", legalDocument.dataset.residence !== newResidence)
         }
     }
 
@@ -18,21 +17,12 @@ if (userTermsBody) {
     }
 
     const autoDetectResidence = () => {
-        const timezoneResidenceMap = new Map([
-            ["Europe/Paris", "france"],
-            ["Europe/Rome", "italy"],
-        ])
+        const timezoneInputMap = new Map([...residenceInputs].map((input) => [input.dataset.timezone, input]))
         const timezoneName = Intl.DateTimeFormat().resolvedOptions().timeZone
-        const residence = timezoneResidenceMap.get(timezoneName)
-        console.debug("Residence from timezone", timezoneName, residence)
+        const residenceInput = timezoneInputMap.get(timezoneName)
+        console.debug("Residence from timezone", timezoneName, residenceInput)
 
-        if (residence) {
-            const residenceInput = userTermsBody.querySelector(`input[name=residence][value=${residence}]`)
-            if (!residenceInput) {
-                console.error("Residence input not found", residence)
-                return
-            }
-
+        if (residenceInput) {
             residenceInput.checked = true
             residenceInput.dispatchEvent(new Event("change"))
         }
