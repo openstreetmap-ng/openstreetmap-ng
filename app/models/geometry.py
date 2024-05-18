5d45bcc4557564bdc1fbc04eaf6f340d9f1bd625
Kamil Monicz <kamil@monicz.dev>
2024-05-18 08:13:40 +0000

    Optimize geometry parsing

--- app/models/geometry.py
+++ app/models/geometry.py
@@ -1,8 +1,10 @@
 from typing import Annotated
 
+import numpy as np
 from annotated_types import Interval
-from shapely import MultiPolygon, Point, Polygon, from_wkb, get_coordinates
+from shapely import MultiPolygon, Point, Polygon, get_coordinates, lib
 from shapely.geometry.base import BaseGeometry
+from shapely.io import DecodingErrorOptions
 from sqlalchemy import BindParameter
 from sqlalchemy.sql import func
 from sqlalchemy.types import UserDefinedType
@@ -18,6 +20,8 @@ Longitude = Annotated[float, Interval(ge=-180, le=180)]
 Latitude = Annotated[float, Interval(ge=-90, le=90)]
 Zoom = Annotated[int, Interval(ge=0, le=25)]
 # TODO: test if type matches after validation
+
+_invalid_handler = np.uint8(DecodingErrorOptions.get_value('raise'))
 
 
 class PointType(UserDefinedType):
@@ -45,7 +49,7 @@ class PointType(UserDefinedType):
         def process(value: bytes | None):
             if value is None:
                 return None
-            return from_wkb(value)
+            return lib.from_wkb(np.asarray(value, dtype=object), _invalid_handler)
 
         return process
 
@@ -74,6 +78,6 @@ class PolygonType(UserDefinedType):
         def process(value: bytes | None):
             if value is None:
                 return None
-            return from_wkb(value)
+            return lib.from_wkb(np.asarray(value, dtype=object), _invalid_handler)
 
         return process
