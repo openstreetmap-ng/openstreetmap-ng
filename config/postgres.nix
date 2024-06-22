345792c3db5596764adfa03c28562e703cfd53ad
Kamil Monicz <kamil@monicz.dev>
2024-06-22 15:30:58 +0200

    Run format

--- config/postgres.nix
+++ config/postgres.nix
@@ -8,74 +8,74 @@
 # - 300GB SSD
 
 pkgs.writeText "postgres.conf" (''
-# listen on socket
-# reason: reduce latency
-unix_socket_directories = '${project_root}/data/postgres_unix'
+  # listen on socket
+  # reason: reduce latency
+  unix_socket_directories = '${project_root}/data/postgres_unix'
 
-# increase buffers and memory usage
-shared_buffers = 2GB
-effective_cache_size = 4GB
-work_mem = 64MB
-maintenance_work_mem = 1GB
-vacuum_buffer_usage_limit = 256MB
+  # increase buffers and memory usage
+  shared_buffers = 2GB
+  effective_cache_size = 4GB
+  work_mem = 64MB
+  maintenance_work_mem = 1GB
+  vacuum_buffer_usage_limit = 256MB
 
-# increase parallelism
-max_parallel_workers_per_gather = 4
+  # increase parallelism
+  max_parallel_workers_per_gather = 4
 
-# allow maintenance to use all workers
-max_parallel_maintenance_workers = 8
+  # allow maintenance to use all workers
+  max_parallel_maintenance_workers = 8
 
-# increase statistics target
-# reason: more accurate query plans
-default_statistics_target = 500
+  # increase statistics target
+  # reason: more accurate query plans
+  default_statistics_target = 500
 
-# increase max connections
-max_connections = 10000
+  # increase max connections
+  max_connections = 10000
 
-# detect disconnected clients
-# reason: safeguard resource usage
-client_connection_check_interval = 5s
+  # detect disconnected clients
+  # reason: safeguard resource usage
+  client_connection_check_interval = 5s
 
-# disconnect idle clients with open transactions
-# reason: safeguard resource usage
-idle_in_transaction_session_timeout = 5min
+  # disconnect idle clients with open transactions
+  # reason: safeguard resource usage
+  idle_in_transaction_session_timeout = 5min
 
-# disable replication and reduce WAL usage
-# reason: unused, reduced resource usage
-wal_level = minimal
-max_wal_senders = 0
+  # disable replication and reduce WAL usage
+  # reason: unused, reduced resource usage
+  wal_level = minimal
+  max_wal_senders = 0
 
-# compress WAL logs
-# reason: reduced IO usage, higher throughput
-wal_compression = zstd
+  # compress WAL logs
+  # reason: reduced IO usage, higher throughput
+  wal_compression = zstd
 
-# group WAL commits during high load (delay 20ms)
-# reason: higher throughput
-commit_delay = 20000
-commit_siblings = 5
+  # group WAL commits during high load (delay 20ms)
+  # reason: higher throughput
+  commit_delay = 20000
+  commit_siblings = 5
 
-# reduce checkpoint frequency
-# reason: higher chance of vaccuming in-memory, reduced WAL usage
-checkpoint_timeout = 1h
+  # reduce checkpoint frequency
+  # reason: higher chance of vaccuming in-memory, reduced WAL usage
+  checkpoint_timeout = 1h
 
-# print early checkpoint warnings
-# reason: detect too-frequent checkpoints
-checkpoint_warning = 30min
+  # print early checkpoint warnings
+  # reason: detect too-frequent checkpoints
+  checkpoint_warning = 30min
 
-# increase max WAL size
-# reason: reduce checkpoint frequency
-max_wal_size = 20GB
+  # increase max WAL size
+  # reason: reduce checkpoint frequency
+  max_wal_size = 20GB
 
-# limit min WAL size
-# reason: prefer file recycling
-min_wal_size = 1GB
+  # limit min WAL size
+  # reason: prefer file recycling
+  min_wal_size = 1GB
 
-# adjust configuration for SSDs
-# reason: improved performance on expected hardware
-random_page_cost = 1
+  # adjust configuration for SSDs
+  # reason: improved performance on expected hardware
+  random_page_cost = 1
 '' + pkgs.lib.optionalString (!pkgs.stdenv.isDarwin) ''
-effective_io_concurrency = 200
-maintenance_io_concurrency = 200
+  effective_io_concurrency = 200
+  maintenance_io_concurrency = 200
 '' + ''
 
 # increase logging verbosity
--- config/supervisord.nix
+++ config/supervisord.nix
@@ -4,44 +4,44 @@ let
   postgresConf = import ./postgres.nix { inherit pkgs project_root; };
 in
 pkgs.writeText "supervisord.conf" ''
-[supervisord]
-logfile=data/supervisor/supervisord.log
-pidfile=data/supervisor/supervisord.pid
-strip_ansi=true
+  [supervisord]
+  logfile=data/supervisor/supervisord.log
+  pidfile=data/supervisor/supervisord.pid
+  strip_ansi=true
 
-[program:postgres]
-command=postgres -c config_file=${postgresConf} -D data/postgres
-stopsignal=INT
-stdout_logfile=data/supervisor/postgres.log
-stderr_logfile=data/supervisor/postgres.log
+  [program:postgres]
+  command=postgres -c config_file=${postgresConf} -D data/postgres
+  stopsignal=INT
+  stdout_logfile=data/supervisor/postgres.log
+  stderr_logfile=data/supervisor/postgres.log
 
-[program:valkey]
-command=valkey-server config/valkey.conf
-stopsignal=INT
-stdout_logfile=data/supervisor/valkey.log
-stderr_logfile=data/supervisor/valkey.log
+  [program:valkey]
+  command=valkey-server config/valkey.conf
+  stopsignal=INT
+  stdout_logfile=data/supervisor/valkey.log
+  stderr_logfile=data/supervisor/valkey.log
 
-[program:mailpit]
-command=mailpit -d data/mailpit/mailpit.db --enable-spamassassin spamassassin.monicz.dev:783 --smtp-auth-accept-any --smtp-auth-allow-insecure
-stopsignal=INT
-stdout_logfile=data/supervisor/mailpit.log
-stderr_logfile=data/supervisor/mailpit.log
+  [program:mailpit]
+  command=mailpit -d data/mailpit/mailpit.db --enable-spamassassin spamassassin.monicz.dev:783 --smtp-auth-accept-any --smtp-auth-allow-insecure
+  stopsignal=INT
+  stdout_logfile=data/supervisor/mailpit.log
+  stderr_logfile=data/supervisor/mailpit.log
 
-[program:watch-js]
-command=watch-js
-stopsignal=TERM
-stdout_logfile=data/supervisor/watch-js.log
-stderr_logfile=data/supervisor/watch-js.log
+  [program:watch-js]
+  command=watch-js
+  stopsignal=TERM
+  stdout_logfile=data/supervisor/watch-js.log
+  stderr_logfile=data/supervisor/watch-js.log
 
-[program:watch-locale]
-command=watch-locale
-stopsignal=TERM
-stdout_logfile=data/supervisor/watch-locale.log
-stderr_logfile=data/supervisor/watch-locale.log
+  [program:watch-locale]
+  command=watch-locale
+  stopsignal=TERM
+  stdout_logfile=data/supervisor/watch-locale.log
+  stderr_logfile=data/supervisor/watch-locale.log
 
-[program:watch-sass]
-command=watch-sass
-stopsignal=TERM
-stdout_logfile=data/supervisor/watch-sass.log
-stderr_logfile=data/supervisor/watch-sass.log
+  [program:watch-sass]
+  command=watch-sass
+  stopsignal=TERM
+  stdout_logfile=data/supervisor/watch-sass.log
+  stderr_logfile=data/supervisor/watch-sass.log
 ''
