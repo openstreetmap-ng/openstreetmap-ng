3f52c0cb5f54d1886b163aa1bcb57101c6a9fc42
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2024-07-07 23:10:16 +0200

    added lang

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -9,10 +9,9 @@ from app.config import APP_URL
 from app.format import FormatRSS06
 from app.lib.auth_context import auth_user, web_user
 from app.lib.date_utils import utcnow
-from app.lib.jinja_env import render
 from app.lib.local_chapters import LOCAL_CHAPTERS
 from app.lib.render_response import render_response
-from app.lib.translation import t
+from app.lib.translation import primary_translation_language, t
 from app.limits import CHANGESET_QUERY_DEFAULT_LIMIT, CHANGESET_QUERY_MAX_LIMIT
 from app.models.db.user import User
 from app.queries.changeset_query import ChangesetQuery
@@ -116,7 +115,7 @@ async def history_feed(
     fg.title(t('changesets.index.title'))
     fg.logo(f'{APP_URL}/static/img/favicon/logo.svg')
     fg.icon(f'{APP_URL}/static/img/favicon/64.webp')
-    # fg.language() TODO: add lang (from where it could be gained)
+    fg.language(primary_translation_language())
     fg.id(f'{APP_URL}/history/feed')
     fg.updated(utcnow())
     fg.rights('CC BY-SA 2.0')
