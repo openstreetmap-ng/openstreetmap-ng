dab42c8881061eac59f08b937a5cf3cb07216483
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2024-07-07 20:11:14 +0200

    replaced anyio with asyncio

--- app/format/api06_changeset_rss.py
+++ app/format/api06_changeset_rss.py
@@ -1,7 +1,6 @@
+from asyncio import TaskGroup
 from collections.abc import Sequence
 
-from anyio import TASK_STATUS_IGNORED, create_task_group
-from anyio.abc import TaskStatus
 from feedgen.feed import FeedGenerator
 
 from app.config import APP_URL
@@ -19,20 +18,15 @@ class ChangesetRSS06Mixin:
         """
         Encode changesets into a feed.
         """
-
-        async with create_task_group() as tg:
+        async with TaskGroup() as tg:
             for changeset in changesets:
-                await tg.start(_encode_changeset, fg, changeset)
+                await tg.create_task(_encode_changeset(fg, changeset))
 
 
 async def _encode_changeset(
     fg: FeedGenerator,
     changeset: Changeset,
-    task_status: TaskStatus = TASK_STATUS_IGNORED,
 ) -> None:
-    # use task_status to preserve the order of the changesets
-    task_status.started()
-
     fe = fg.add_entry(order='append')
     fe.title(f'{t("browse.changeset.feed.title",id=changeset.id)}')
     fe.id(f'{APP_URL}/changeset/{changeset.id}')
