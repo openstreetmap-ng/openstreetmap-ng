510ae06a6b35c3ac84f99c66de7db37b6817300f
Kamil Monicz <kamil@monicz.dev>
2024-07-08 21:06:37 +0000

    Simplify get_history_feed

--- app/lib/feed.py
+++ app/lib/feed.py
@@ -1,38 +1,41 @@
-from typing import Annotated
-
-from fastapi import Query
+from fastapi import Response
 from feedgen.feed import FeedGenerator
+from sqlalchemy.orm import joinedload
 from starlette import status
-from starlette.responses import HTMLResponse
 
 from app.config import APP_URL, ATTRIBUTION_URL
 from app.format import FormatRSS06
 from app.lib.date_utils import utcnow
+from app.lib.options_context import options_context
 from app.lib.translation import primary_translation_language, t
-from app.limits import CHANGESET_QUERY_DEFAULT_LIMIT, CHANGESET_QUERY_MAX_LIMIT
+from app.models.db.changeset import Changeset
+from app.models.db.user import User
 from app.queries.changeset_query import ChangesetQuery
 from app.queries.user_query import UserQuery
 
 
 async def get_history_feed(
+    *,
     user_display_name: str | None = None,
-    limit: Annotated[int, Query(gt=0, le=CHANGESET_QUERY_MAX_LIMIT)] = CHANGESET_QUERY_DEFAULT_LIMIT,
+    limit: int,
 ):
-    if user_display_name:
+    if user_display_name is not None:
         user = await UserQuery.find_one_by_display_name(display_name=user_display_name)
-        if not user:
-            return HTMLResponse(status_code=status.HTTP_404_NOT_FOUND, media_type='application/atom+xml')
-        else:
-            user_id = user.id
+        if user is None:
+            return Response(None, status.HTTP_404_NOT_FOUND, media_type='application/atom+xml')
+        user_id = user.id
     else:
         user_id = None
-    changesets = await ChangesetQuery.find_many_by_query(
-        user_id=user_id,
-        limit=limit,
-    )
-    if len(changesets) == 0:
-        return HTMLResponse(status_code=status.HTTP_404_NOT_FOUND, media_type='application/atom+xml')
+
+    with options_context(joinedload(Changeset.user).load_only(User.display_name)):
+        changesets = await ChangesetQuery.find_many_by_query(user_id=user_id, limit=limit)
+    if not changesets:
+        return Response(None, status.HTTP_404_NOT_FOUND, media_type='application/atom+xml')
+
     fg = FeedGenerator()
+    fg.language(primary_translation_language())
+    fg.id(f'{APP_URL}/history/feed')
+    fg.updated(utcnow())
     fg.link(rel='self', type='text/html', href=f'{APP_URL}/history/feed')
     fg.link(
         rel='alternate',
@@ -40,11 +43,8 @@ async def get_history_feed(
         href=f'{APP_URL}/history/feed',
     )
     fg.title(t('changesets.index.title'))
-    fg.logo(f'{APP_URL}/static/img/favicon/logo.svg')
     fg.icon(f'{APP_URL}/static/img/favicon/64.webp')
-    fg.language(primary_translation_language())
-    fg.id(f'{APP_URL}/history/feed')
-    fg.updated(utcnow())
+    fg.logo(f'{APP_URL}/static/img/favicon/256.webp')
     fg.rights(ATTRIBUTION_URL)
     await FormatRSS06.encode_changesets(fg, changesets)
     return fg.atom_str(pretty=True)
