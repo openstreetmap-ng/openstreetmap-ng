16722deb56e7441a97ddd0a58a12a291d9a34908
Filip Czaplicki <github@starsep.com>
2024-07-10 20:46:25 +0200

    Locale path override for about and copyright

--- app/controllers/index.py
+++ app/controllers/index.py
@@ -2,11 +2,14 @@ from typing import Annotated
 
 from fastapi import APIRouter
 from starlette import status
-from starlette.responses import FileResponse, RedirectResponse
+from starlette.responses import FileResponse, RedirectResponse, Response
 
+from app.config import DEFAULT_LANGUAGE
 from app.lib.auth_context import auth_user, web_user
 from app.lib.local_chapters import LOCAL_CHAPTERS
+from app.lib.locale import is_valid_locale
 from app.lib.render_response import render_response
+from app.lib.translation import primary_translation_language, t, translation_context
 from app.models.db.user import User
 
 router = APIRouter()
@@ -47,9 +50,30 @@ async def communities():
     return render_response('communities.jinja2', {'local_chapters': LOCAL_CHAPTERS})
 
 
+@router.get('/copyright/{locale:str}')
+async def copyright_i18n(locale: str):
+    if not is_valid_locale(locale):
+        return Response(None, status.HTTP_404_NOT_FOUND)
+    with translation_context(primary_translation_language()):
+        copyright_notice_title = t('site.copyright.foreign.title')
+        original_copyright_link_text = t('site.copyright.foreign.english_link')
+        original_copyright_link = f'<a href="/copyright/en">{original_copyright_link_text}</a>'
+        copyright_notice_html = t('site.copyright.foreign.html', english_original_link=original_copyright_link)
+    with translation_context(locale):
+        return render_response(
+            'copyright.jinja2',
+            dict(
+                should_show_notice=locale != primary_translation_language()
+                or primary_translation_language() != DEFAULT_LANGUAGE,
+                copyright_notice_title=copyright_notice_title,
+                copyright_notice_html=copyright_notice_html,
+            ),
+        )
+
+
 @router.get('/copyright')
 async def copyright_():
-    return render_response('copyright.jinja2')
+    return await copyright_i18n(locale=primary_translation_language())
 
 
 @router.get('/help')
@@ -57,9 +81,17 @@ async def help_():
     return render_response('help.jinja2')
 
 
+@router.get('/about/{locale:str}')
+async def about_i18n(locale: str):
+    if not is_valid_locale(locale):
+        return Response(None, status.HTTP_404_NOT_FOUND)
+    with translation_context(locale):
+        return render_response('about.jinja2')
+
+
 @router.get('/about')
 async def about():
-    return render_response('about.jinja2')
+    return await about_i18n(locale=primary_translation_language())
 
 
 @router.get('/fixthemap')
