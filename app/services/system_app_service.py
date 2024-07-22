4bf604b0674d71cb204f7255ac9c5cdee2abf54b
Kamil Monicz <kamil@monicz.dev>
2024-07-22 09:55:16 +0000

    Faster system app startup

--- app/services/system_app_service.py
+++ app/services/system_app_service.py
@@ -1,4 +1,5 @@
 import logging
+from asyncio import TaskGroup
 
 from sqlalchemy import func
 from sqlalchemy.dialects.postgresql import insert
@@ -10,7 +11,7 @@ from app.lib.crypto import hash_bytes
 from app.lib.exceptions_context import raise_for
 from app.models.db.oauth2_application import OAuth2Application
 from app.models.db.oauth2_token import OAuth2Token
-from app.models.system_app import SYSTEM_APPS
+from app.models.system_app import SYSTEM_APPS, SystemApp
 from app.queries.oauth2_application_query import OAuth2ApplicationQuery
 
 
@@ -20,32 +21,9 @@ class SystemAppService:
         """
         Register and update system apps in the database.
         """
-        async with db_commit() as session:
+        async with TaskGroup() as tg:
             for app in SYSTEM_APPS:
-                logging.info('Registering system app %r', app.name)
-                stmt = (
-                    insert(OAuth2Application)
-                    .values(
-                        {
-                            OAuth2Application.user_id: None,
-                            OAuth2Application.name: app.name,
-                            OAuth2Application.client_id: app.client_id,
-                            OAuth2Application.client_secret_encrypted: b'',
-                            OAuth2Application.scopes: app.scopes,
-                            OAuth2Application.is_confidential: True,
-                            OAuth2Application.redirect_uris: (),
-                        }
-                    )
-                    .on_conflict_do_update(
-                        index_elements=(OAuth2Application.client_id,),
-                        set_={
-                            OAuth2Application.name: app.name,
-                            OAuth2Application.scopes: app.scopes,
-                        },
-                    )
-                    .inline()
-                )
-                await session.execute(stmt)
+                tg.create_task(_register_app(app))
 
     @staticmethod
     async def create_access_token(client_id: str, *, user_id: int | None = None) -> str:
@@ -74,3 +52,34 @@ class SystemAppService:
             token.authorized_at = func.statement_timestamp()
             session.add(token)
         return access_token
+
+
+async def _register_app(app: SystemApp) -> None:
+    """
+    Register a system app.
+    """
+    logging.info('Registering system app %r', app.name)
+    async with db_commit() as session:
+        stmt = (
+            insert(OAuth2Application)
+            .values(
+                {
+                    OAuth2Application.user_id: None,
+                    OAuth2Application.name: app.name,
+                    OAuth2Application.client_id: app.client_id,
+                    OAuth2Application.client_secret_encrypted: b'',
+                    OAuth2Application.scopes: app.scopes,
+                    OAuth2Application.is_confidential: True,
+                    OAuth2Application.redirect_uris: (),
+                }
+            )
+            .on_conflict_do_update(
+                index_elements=(OAuth2Application.client_id,),
+                set_={
+                    OAuth2Application.name: app.name,
+                    OAuth2Application.scopes: app.scopes,
+                },
+            )
+            .inline()
+        )
+        await session.execute(stmt)
