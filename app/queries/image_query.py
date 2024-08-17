f07d9bb061957e6c12843867c987a950472c9a66
Kamil Monicz <kamil@monicz.dev>
2024-08-17 23:21:30 +0000

    Raise exceptions on gravatar fetches without proper avatar type

--- app/queries/image_query.py
+++ app/queries/image_query.py
@@ -1,4 +1,5 @@
 from app.lib.exceptions_context import raise_for
+from app.lib.image import AvatarType
 from app.models.types import StorageKey
 from app.queries.user_query import UserQuery
 from app.storage import AVATAR_STORAGE, BACKGROUND_STORAGE, GRAVATAR_STORAGE
@@ -8,11 +9,13 @@ class ImageQuery:
     @staticmethod
     async def get_gravatar(user_id: int) -> bytes:
         """
-        Get a user's gravatar image.
+        Get a user's gravatar image.r
         """
         user = await UserQuery.find_one_by_id(user_id)
         if user is None:
             raise_for().user_not_found(user_id)
+        if user.avatar_type != AvatarType.gravatar:
+            raise_for().image_not_found()
         return await GRAVATAR_STORAGE.load(user.email)
 
     @staticmethod
@@ -23,7 +26,7 @@ class ImageQuery:
         try:
             return await AVATAR_STORAGE.load(avatar_id)
         except FileNotFoundError:
-            raise_for().image_not_found(avatar_id)
+            raise_for().image_not_found()
 
     @staticmethod
     async def get_background(background_id: StorageKey) -> bytes:
@@ -33,4 +36,4 @@ class ImageQuery:
         try:
             return await BACKGROUND_STORAGE.load(background_id)
         except FileNotFoundError:
-            raise_for().image_not_found(background_id)
+            raise_for().image_not_found()
