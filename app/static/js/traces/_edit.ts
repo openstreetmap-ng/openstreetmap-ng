d4647f33c33ea3b111ca522c8324c4139d826544
Kamil Monicz <kamil@monicz.dev>
2024-11-16 11:11:26 +0000

    Uniform trace editing

--- app/static/js/traces/_edit.ts
+++ app/static/js/traces/_edit.ts
@@ -1,3 +1,4 @@
+import { t } from "i18next"
 import { configureStandardForm } from "../_standard-form"
 
 const body = document.querySelector("body.traces-edit-body")
@@ -7,5 +8,20 @@ if (body) {
         // On success callback, navigate to the trace details
         console.debug("onUpdateFormSuccess", trace_id)
         window.location.href = `/trace/${trace_id}`
+    })
+
+    const deleteForm = body.querySelector("form.delete-form")
+    configureStandardForm(deleteForm, ({ redirect_url }) => {
+        // On success callback, navigate to my traces
+        console.debug("onDeleteFormSuccess", redirect_url)
+        window.location.href = redirect_url
+    })
+
+    // On delete button click, request confirmation
+    const deleteButton = deleteForm.querySelector("button[type=submit]")
+    deleteButton.addEventListener("click", (event: Event) => {
+        if (!confirm(t("trace.delete_confirmation"))) {
+            event.preventDefault()
+        }
     })
 }
