91360d3b3ee45bc422987d83e80537c872dea266
Kamil Monicz <kamil@monicz.dev>
2025-01-09 19:46:50 +0000

    Restore scroll position on sidebar reload, Resolves #145
    Co-authored-by: soniya malviya <151515611+soniya-malviy@users.noreply.github.com>

--- app/static/js/index/_base-fetch.ts
+++ app/static/js/index/_base-fetch.ts
@@ -1,6 +1,10 @@
 import type * as L from "leaflet"
 import { resolveDatetime } from "../_datetime"
+import { requestAnimationFramePolyfill } from "../_utils"
 import { configureActionSidebar, getActionSidebar, switchActionSidebar } from "./_action-sidebar"
+
+let currentUrl: string | null = null
+let sidebarScrollPosition = 0
 
 export interface BaseFetchController {
     load: ({ url }: { url: string | null }) => void
@@ -14,6 +18,7 @@ export const getBaseFetchController = (
     successCallback?: (dynamicContent: HTMLElement) => void,
 ): BaseFetchController => {
     const sidebar = getActionSidebar(className)
+    const scrollSidebar = sidebar.closest(".sidebar")
     const dynamicContent = sidebar.classList.contains("dynamic-content")
         ? sidebar
         : sidebar.querySelector("div.dynamic-content")
@@ -23,20 +28,49 @@ export const getBaseFetchController = (
 
     /** On sidebar loading, display loading content */
     const onSidebarLoading = () => {
+        sidebarScrollPosition = scrollSidebar.scrollTop
+        console.debug("Save sidebar scroll position", sidebarScrollPosition)
         dynamicContent.innerHTML = loadingHtml
     }
 
     /** On sidebar loaded, display content and call callback */
-    const onSidebarLoaded = (html: string): void => {
+    const onSidebarLoaded = (html: string, newUrl: string): void => {
         dynamicContent.innerHTML = html
         resolveDatetime(dynamicContent)
         configureActionSidebar(sidebar)
+
+        if (currentUrl === newUrl) {
+            // If reload, restore sidebar scroll position
+            const startTime = performance.now()
+            const tryRestoreScroll = () => {
+                if (scrollSidebar.scrollHeight > sidebarScrollPosition) {
+                    scrollSidebar.scrollTop = sidebarScrollPosition
+                    console.debug("Restore sidebar scroll position", sidebarScrollPosition)
+                    return
+                }
+                if (performance.now() - startTime < 2000) {
+                    requestAnimationFramePolyfill(tryRestoreScroll)
+                } else {
+                    console.warn("Failed to restore sidebar scroll: content too small", {
+                        target: sidebarScrollPosition,
+                        actual: scrollSidebar.scrollHeight,
+                        newUrl,
+                    })
+                }
+            }
+            requestAnimationFramePolyfill(tryRestoreScroll)
+        } else {
+            currentUrl = newUrl
+        }
     }
 
     return {
         load: ({ url }) => {
             switchActionSidebar(map, className)
-            if (!url) return
+            if (!url) {
+                currentUrl = url
+                return
+            }
 
             // Abort any pending request
             abortController?.abort()
@@ -54,7 +88,7 @@ export const getBaseFetchController = (
                 .then(async (resp) => {
                     if (!resp.ok && resp.status !== 404) throw new Error(`${resp.status} ${resp.statusText}`)
 
-                    onSidebarLoaded(await resp.text())
+                    onSidebarLoaded(await resp.text(), url)
                     successCallback?.(dynamicContent)
                 })
                 .catch((error) => {
