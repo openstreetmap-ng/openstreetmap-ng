aefb7dcd4fa45108dc26cac5e1653a09b3a7eb68
Kamil Monicz <kamil@monicz.dev>
2025-03-23 07:44:53 +0000

    Small test_route_conflicts optimization

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,10 +10,8 @@ def test_route_conflicts():
     capture_paths: defaultdict[str, list[re.Pattern[str]]] = defaultdict(list)
 
     for route in main.routes:
-        if not isinstance(route, Route):
+        if not isinstance(route, Route) or not (route_methods := route.methods):
             continue
-
-        route_methods = route.methods or ()
 
         for p in (p for method in route_methods for p in capture_paths[method]):
             assert not p.match(route.path), f'Route {route.path!r} is ordered after matching {p.pattern!r}'
