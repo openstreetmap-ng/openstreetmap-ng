5324cb49fd4b6188833b5dd1a74ea2703dffd424
Kamil Monicz <kamil@monicz.dev>
2025-04-30 21:20:16 +0000

    Optimize XMLToDict parsing

--- app/lib/xmltodict.py
+++ app/lib/xmltodict.py
@@ -1,11 +1,10 @@
 import logging
 from collections.abc import Callable
 from datetime import UTC, datetime
-from typing import Any, Literal, Protocol, overload
+from typing import TYPE_CHECKING, Any, Literal, Protocol, overload
 
 import cython
-import lxml.etree as tree
-from lxml.etree import CDATA, Element
+from lxml.etree import CDATA, Element, XMLParser, tostring
 from sizestr import sizestr
 
 from app.config import XML_PARSE_MAX_SIZE
@@ -13,7 +12,10 @@ from app.lib.date_utils import parse_date
 from app.lib.exceptions_context import raise_for
 from app.lib.format_style_context import format_is_json
 
-_PARSER = tree.XMLParser(
+if TYPE_CHECKING:
+    from lxml.etree import _Element
+
+_PARSER = XMLParser(
     recover=True,
     remove_blank_text=True,
     remove_comments=True,
@@ -27,21 +29,21 @@ class XMLToDict:
     @staticmethod
     def parse(
         xml_bytes: bytes, *, size_limit: int | None = XML_PARSE_MAX_SIZE
-    ) -> dict[str, list[tuple[str, Any]] | dict[str, Any]]:
+    ) -> dict[str, dict[str, Any] | list[tuple[str, Any]]]:
         """Parse XML string to dict."""
         if size_limit is not None and len(xml_bytes) > size_limit:
             raise_for.input_too_big(len(xml_bytes))
 
         logging.debug('Parsing %s XML string', sizestr(len(xml_bytes)))
-        root = tree.fromstring(xml_bytes, parser=_PARSER)
-        root_element = _parse_element(root)
+        _PARSER.feed(xml_bytes)
+        root = _PARSER.close()
+        k = _strip_namespace(root.tag)
+        v = _parse_element(k, root)
 
-        if isinstance(root_element, str):  # type: ignore
-            raise_for.bad_xml(
-                root.tag, f'XML contains only text: {root_element}', xml_bytes
-            )
+        if not isinstance(v, (dict, list)):
+            raise_for.bad_xml(k, f'XML contains only text: {v}', xml_bytes)
 
-        return {_strip_namespace(root.tag): root_element}
+        return {k: v}
 
     @staticmethod
     @overload
@@ -66,124 +68,9 @@ class XMLToDict:
         if root_element is None:
             root_element = Element(root_k)
 
-        result = tree.tostring(root_element, encoding='UTF-8', xml_declaration=True)
+        result = tostring(root_element, encoding='UTF-8', xml_declaration=True)
         logging.debug('Unparsed %s XML string', sizestr(len(result)))
         return result if raw else result.decode()
-
-
-@cython.cfunc
-def _parse_element(element: tree._Element):
-    # parse attributes
-    # read property once for performance
-    value_postprocessor: dict[str, Callable[[str], Any]] = _VALUE_POSTPROCESSOR
-    parsed: list[tuple[str, Any]] = [
-        (f'@{k}', call(v) if (call := value_postprocessor.get(k)) is not None else v)  # pyright: ignore[reportArgumentType]
-        for k, v in element.attrib.items()
-    ]
-
-    # parse children
-    sequence_mark: cython.bint = False
-    if children := list(element):
-        # read property once for performance
-        force_sequence_root: set[str] = _FORCE_SEQUENCE_ROOT
-        force_list: set[str] = _FORCE_LIST
-        parsed_children: dict[str, Any | list[Any]] = {}
-
-        for child in children:
-            k = _strip_namespace(child.tag)
-            v = _parse_element(child)
-
-            if isinstance(v, str) and (call := value_postprocessor.get(k)) is not None:  # pyright: ignore [reportUnnecessaryIsInstance]
-                v = call(v)
-
-            # in sequence mode, return root element as tuple
-            if k in force_sequence_root:
-                parsed.append((k, v))
-                sequence_mark = True
-
-            # merge with existing value
-            elif (parsed_v := parsed_children.get(k)) is not None:
-                if isinstance(parsed_v, list):
-                    parsed_v.append(v)
-                else:
-                    # upgrade from single value to list
-                    parsed_children[k] = [parsed_v, v]
-
-            # add new value
-            else:
-                parsed_children[k] = [v] if k in force_list else v
-
-        if parsed_children:
-            parsed.extend(parsed_children.items())
-
-    # parse text content
-    if (text := element.text) is not None and (text := text.strip()):
-        if not parsed:
-            return text
-        parsed.append(('#text', text))
-
-    # in sequence mode, return element as tuple
-    return parsed if sequence_mark else dict(parsed)
-
-
-@cython.cfunc
-def _unparse_element(key: str, value: Any) -> list[tree._Element]:
-    k: str
-    v: Any
-
-    # encode dict
-    if isinstance(value, dict):
-        element = Element(key)
-        element_attrib = element.attrib  # read property once for performance
-        element_extend = element.extend
-        for k, v in value.items():
-            if k[0] == '@':
-                element_attrib[k[1:]] = _to_string(v)
-            elif k == '#text':
-                element.text = _to_string(v)
-            else:
-                element_extend(_unparse_element(k, v))
-        return [element]
-
-    # encode sequence of ...
-    elif isinstance(value, (list, tuple)):
-        if not value:
-            return []
-        first = value[0]
-
-        # encode sequence of dicts
-        if isinstance(first, dict):
-            return [e for v in value for e in _unparse_element(key, v)]
-
-        # encode sequence of (key, value) tuples
-        elif isinstance(first, (list, tuple)):
-            element = Element(key)
-            element_attrib = element.attrib  # read property once for performance
-            element_extend = element.extend
-            for k, v in value:
-                if k[0] == '@':
-                    element_attrib[k[1:]] = _to_string(v)
-                elif k == '#text':
-                    element.text = _to_string(v)
-                else:
-                    element_extend(_unparse_element(k, v))
-            return [element]
-
-        # encode sequence of scalars
-        else:
-            result: list[tree._Element] = [None] * len(value)  # type: ignore
-            i: cython.Py_ssize_t
-            for i, v in enumerate(value):
-                element = Element(key)
-                element.text = _to_string(v)
-                result[i] = element
-            return result
-
-    # encode scalar
-    else:
-        element = Element(key)
-        element.text = _to_string(value)
-        return [element]
 
 
 # tags that will become tuples (order-preserving): [('tag', ...), ('tag', ...), ...]
@@ -278,6 +165,127 @@ def _to_string(v: Any):
 @cython.cfunc
 def _strip_namespace(tag: str) -> str:
     return tag.rsplit('}', 1)[-1] if tag[0] == '{' else tag
+
+
+@cython.cfunc
+def _parse_element(
+    element_tag_strip: str,
+    element: _Element,
+    /,
+    *,
+    value_postprocessor: dict[str, Callable[[str], Any]] = _VALUE_POSTPROCESSOR,
+    force_sequence_root: set[str] = _FORCE_SEQUENCE_ROOT,
+    force_list: set[str] = _FORCE_LIST,
+):
+    k: str
+    v: Any
+
+    # parse attributes
+    parsed: dict[str, Any | list[Any]] = {
+        '@' + k: call(v) if (call := value_postprocessor.get(k)) is not None else v  # type: ignore
+        for k, v in element.attrib.items()
+    }
+
+    # parse children
+    parsed_seq: list[tuple[str, Any]] | None = None
+
+    for child in element:
+        k = _strip_namespace(child.tag)
+        v = _parse_element(k, child)
+
+        # in sequence mode, return root element as list
+        if k in force_sequence_root:
+            if parsed_seq is not None:
+                parsed_seq.append((k, v))
+            else:
+                parsed_seq = [(k, v)]
+
+        # merge with existing value
+        elif (parsed_v := parsed.get(k)) is not None:
+            if isinstance(parsed_v, list):
+                parsed_v.append(v)
+            else:
+                # upgrade from single value to list
+                parsed[k] = [parsed_v, v]
+
+        # add new value
+        else:
+            parsed[k] = [v] if k in force_list else v
+
+    # parse text content
+    if (text := element.text) is not None and (text := text.strip()):
+        if (call := value_postprocessor.get(element_tag_strip)) is not None:
+            text = call(text)
+        if not parsed and parsed_seq is None:
+            return text
+        parsed['#text'] = text
+
+    # in sequence mode, return element as list
+    if parsed_seq is None:
+        return parsed
+    if parsed:
+        parsed_seq.extend(parsed.items())
+    return parsed_seq
+
+
+@cython.cfunc
+def _unparse_element(key: str, value: Any) -> list[_Element]:
+    k: str
+    v: Any
+
+    # encode dict
+    if isinstance(value, dict):
+        element = Element(key)
+        element_attrib = element.attrib  # read property once for performance
+        element_extend = element.extend
+        for k, v in value.items():
+            if k[0] == '@':
+                element_attrib[k[1:]] = _to_string(v)
+            elif k == '#text':
+                element.text = _to_string(v)
+            else:
+                element_extend(_unparse_element(k, v))
+        return [element]
+
+    # encode sequence of ...
+    elif isinstance(value, (list, tuple)):
+        if not value:
+            return []
+        first = value[0]
+
+        # encode sequence of dicts
+        if isinstance(first, dict):
+            return [e for v in value for e in _unparse_element(key, v)]
+
+        # encode sequence of (key, value) tuples
+        elif isinstance(first, (list, tuple)):
+            element = Element(key)
+            element_attrib = element.attrib  # read property once for performance
+            element_extend = element.extend
+            for k, v in value:
+                if k[0] == '@':
+                    element_attrib[k[1:]] = _to_string(v)
+                elif k == '#text':
+                    element.text = _to_string(v)
+                else:
+                    element_extend(_unparse_element(k, v))
+            return [element]
+
+        # encode sequence of scalars
+        else:
+            result: list[_Element] = [None] * len(value)  # type: ignore
+            i: cython.Py_ssize_t
+            for i, v in enumerate(value):
+                element = Element(key)
+                element.text = _to_string(v)
+                result[i] = element
+            return result
+
+    # encode scalar
+    else:
+        element = Element(key)
+        element.text = _to_string(value)
+        return [element]
 
 
 class _XAttrCallable(Protocol):
