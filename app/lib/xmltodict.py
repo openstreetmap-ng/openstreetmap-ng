03c6ba27b3465fce985bf408f66cc561d8227e6f
Kamil Monicz <kamil@monicz.dev>
2025-05-19 01:33:15 +0200

    Remove Cython XML parser implementation

--- app/lib/xmltodict.py
+++ app/lib/xmltodict.py
@@ -1,5 +1,4 @@
 import logging
-from collections.abc import Callable
 from datetime import UTC, datetime
 from typing import TYPE_CHECKING, Any, Literal, Protocol, overload
 
@@ -8,9 +7,9 @@ from lxml.etree import CDATA, XMLParser, tostring
 from sizestr import sizestr
 
 from app.config import XML_PARSE_MAX_SIZE
-from app.lib.date_utils import parse_date
 from app.lib.exceptions_context import raise_for
 from app.lib.format_style_context import format_is_json
+from xmltodict.lib import parse
 
 if TYPE_CHECKING:
     from lxml.etree import _Element
@@ -34,15 +33,11 @@ class XMLToDict:
             raise_for.input_too_big(len(xml_bytes))
 
         logging.debug('Parsing %s XML string', sizestr(len(xml_bytes)))
-        _PARSER.feed(xml_bytes)
-        root = _PARSER.close()
-        k = _strip_namespace(root.tag)
-        v = _parse_element(k, root, {})
 
-        if (t := type(v)) is not dict and t is not list:
-            raise_for.bad_xml(k, f'XML contains only text: {v}', xml_bytes)
-
-        return {k: v}  # type: ignore
+        try:
+            return parse(xml_bytes)
+        except ValueError as e:
+            raise_for.bad_xml('data', str(e), xml_bytes)
 
     @staticmethod
     @overload
@@ -67,81 +62,6 @@ class XMLToDict:
         result = tostring(root, encoding='UTF-8', xml_declaration=True)
         logging.debug('Unparsed %s XML string', sizestr(len(result)))
         return result if raw else result.decode()
-
-
-# tags that will become tuples (order-preserving): [('tag', ...), ('tag', ...), ...]
-_FORCE_SEQUENCE_ROOT: set[str] = {
-    'bounds',
-    'create',
-    'modify',
-    'delete',
-    'node',
-    'way',
-    'relation',
-}
-
-# tags that will become a list of values: {'tag': [...]}
-_FORCE_LIST: set[str] = {
-    'member',
-    'tag',
-    'nd',
-    'trk',
-    'trkseg',
-    'trkpt',
-    'preference',
-    'note',
-    'comment',
-    'gpx_file',
-}
-
-
-@cython.cfunc
-def _strip_namespace(tag: str) -> str:
-    return tag.rsplit('}', 1)[-1] if tag[0] == '{' else tag
-
-
-@cython.cfunc
-def _parse_xml_bool(value: str):
-    return value == 'true'
-
-
-@cython.cfunc
-def _parse_xml_version(value: str):
-    # for simplicity, we don't support floating-point versions
-    return int(value) if '.' not in value else float(value)
-
-
-@cython.cfunc
-def _parse_xml_date(value: str):
-    return datetime.fromisoformat(value) if ' ' not in value else parse_date(value)
-
-
-_VALUE_POSTPROCESSOR: dict[str, Callable[[str], Any]] = {
-    'changeset': int,
-    'changes_count': int,
-    'closed_at': _parse_xml_date,
-    'comments_count': int,
-    'created_at': _parse_xml_date,
-    'date': _parse_xml_date,
-    'id': int,
-    'lat': float,
-    'lon': float,
-    'ele': float,
-    'max_lat': float,
-    'max_lon': float,
-    'min_lat': float,
-    'min_lon': float,
-    'num_changes': int,
-    'open': _parse_xml_bool,
-    'pending': _parse_xml_bool,
-    'ref': int,
-    'time': _parse_xml_date,
-    'timestamp': _parse_xml_date,
-    'uid': int,
-    'updated_at': _parse_xml_date,
-    'version': _parse_xml_version,
-    'visible': _parse_xml_bool,
-}
 
 
 @cython.cfunc
@@ -173,73 +93,6 @@ def _to_string(
         return v.isoformat() + 'Z'
 
     return str(v)
-
-
-@cython.cfunc
-def _parse_element(
-    element_tag_strip: str,
-    element: '_Element',
-    /,
-    attr_cache: dict[str, str],
-    *,
-    value_postprocessor: dict[str, Callable[[str], Any]] = _VALUE_POSTPROCESSOR,
-    force_sequence_root: set[str] = _FORCE_SEQUENCE_ROOT,
-    force_list: set[str] = _FORCE_LIST,
-):
-    # parse attributes
-    parsed: dict[str, Any | list[Any]] = {
-        (
-            k_
-            if (k_ := attr_cache.get(k)) is not None  # type: ignore
-            else attr_cache.setdefault(k, '@' + k)  # type: ignore
-        ): (
-            call(v)  # type: ignore
-            if (call := value_postprocessor.get(k)) is not None  # type: ignore
-            else v
-        )
-        for k, v in element.attrib.items()
-    }
-
-    # parse children
-    parsed_seq: list[tuple[str, Any]] | None = None
-
-    for child in element:
-        k = _strip_namespace(child.tag)
-        v = _parse_element(k, child, attr_cache)
-
-        # in sequence mode, return root element as list
-        if k in force_sequence_root:
-            if parsed_seq is not None:
-                parsed_seq.append((k, v))
-            else:
-                parsed_seq = [(k, v)]
-
-        # merge with existing value
-        elif (parsed_v := parsed.get(k)) is not None:
-            if type(parsed_v) is list:
-                parsed_v.append(v)
-            else:
-                # upgrade from single value to list
-                parsed[k] = [parsed_v, v]
-
-        # add new value
-        else:
-            parsed[k] = [v] if k in force_list else v
-
-    # parse text content
-    if (text := element.text) is not None and (text := text.strip()):
-        if (call := value_postprocessor.get(element_tag_strip)) is not None:
-            text = call(text)
-        if not parsed and parsed_seq is None:
-            return text
-        parsed['#text'] = text
-
-    # in sequence mode, return element as list
-    if parsed_seq is None:
-        return parsed
-    if parsed:
-        parsed_seq.extend(parsed.items())
-    return parsed_seq
 
 
 @cython.cfunc
