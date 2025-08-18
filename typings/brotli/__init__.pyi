5e59ba4ef50e972ef0c56af189d14fa0d31618f3
Kamil Monicz <kamil@monicz.dev>
2025-08-18 10:13:28 +0000

    Add brotli stub file

---
+++ typings/brotli/__init__.pyi
@@ -0,0 +1,36 @@
+# Constants
+MODE_GENERIC: int
+MODE_TEXT: int
+MODE_FONT: int
+version: str
+
+# Exception
+class error(Exception): ...
+
+# Classes
+class Compressor:
+    def __init__(
+        self,
+        mode: int = MODE_GENERIC,
+        quality: int = 11,
+        lgwin: int = 22,
+        lgblock: int = 0,
+    ) -> None: ...
+    def process(self, string: bytes) -> bytes: ...
+    def flush(self) -> bytes: ...
+    def finish(self) -> bytes: ...
+
+class Decompressor:
+    def __init__(self) -> None: ...
+    def process(self, string: bytes) -> bytes: ...
+    def is_finished(self) -> bool: ...
+
+# Functions
+def compress(
+    string: bytes,
+    mode: int = 0,
+    quality: int = 11,
+    lgwin: int = 22,
+    lgblock: int = 0,
+) -> bytes: ...
+def decompress(string: bytes) -> bytes: ...
