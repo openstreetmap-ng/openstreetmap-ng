5e4a042762ba8a4e3200e7da85643a2ce938da13
Kamil Monicz <kamil@monicz.dev>
2025-08-31 19:50:27 +0000

    Consolidate admin users files

--- app/controllers/admin_users.py
+++ app/controllers/admin_users.py
@@ -1,13 +1,19 @@
+from asyncio import TaskGroup
 from datetime import datetime
 from math import ceil
 from typing import Annotated, Literal
 
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Response
+from starlette import status
 
-from app.config import USER_LIST_PAGE_SIZE
+from app.config import DISPLAY_NAME_MAX_LENGTH, PASSWORD_MIN_LENGTH, USER_LIST_PAGE_SIZE
 from app.lib.auth_context import web_user
 from app.lib.render_response import render_response
 from app.models.db.user import User, UserRole
+from app.models.types import UserId
+from app.queries.connected_account_query import ConnectedAccountQuery
+from app.queries.oauth2_application_query import OAuth2ApplicationQuery
+from app.queries.oauth2_token_query import OAuth2TokenQuery
 from app.queries.user_query import UserQuery
 
 router = APIRouter()
@@ -47,5 +53,37 @@ async def users_index(
             'created_after': created_after.isoformat() if created_after else '',
             'created_before': created_before.isoformat() if created_before else '',
             'sort': sort,
+        },
+    )
+
+
+@router.get('/admin/users/{user_id:int}')
+async def user_edit(
+    _: Annotated[User, web_user('role_administrator')],
+    user_id: UserId,
+):
+    edit_user = await UserQuery.find_by_id(user_id)
+    if edit_user is None:
+        return Response(None, status.HTTP_404_NOT_FOUND)
+
+    async with TaskGroup() as tg:
+        connected_providers_task = tg.create_task(
+            ConnectedAccountQuery.get_providers_by_user(user_id)
+        )
+        applications_task = tg.create_task(OAuth2ApplicationQuery.find_by_user(user_id))
+        tokens_task = tg.create_task(OAuth2TokenQuery.find_pats_by_user(user_id))
+        authorizations = await OAuth2TokenQuery.find_unique_per_app_by_user(user_id)
+        tg.create_task(OAuth2ApplicationQuery.resolve_applications(authorizations))
+
+    return await render_response(
+        'admin/users/edit',
+        {
+            'edit_user': edit_user,
+            'connected_providers': connected_providers_task.result(),
+            'authorizations': authorizations,
+            'applications': applications_task.result(),
+            'tokens': tokens_task.result(),
+            'DISPLAY_NAME_MAX_LENGTH': DISPLAY_NAME_MAX_LENGTH,
+            'PASSWORD_MIN_LENGTH': PASSWORD_MIN_LENGTH,
         },
     )
--- app/controllers/web_admin_users.py
+++ app/controllers/web_admin_users.py
@@ -2,16 +2,24 @@ from datetime import datetime, timedelta
 from typing import Annotated, Literal
 
 import orjson
-from fastapi import APIRouter, Query, Response
+from fastapi import APIRouter, Form, Query, Response
 from pydantic import PositiveInt
+from starlette import status
+from starlette.responses import RedirectResponse
 
-from app.config import USER_EXPORT_LIMIT
+from app.config import ENV, USER_EXPORT_LIMIT
 from app.lib.auth_context import web_user
 from app.lib.render_response import render_response
+from app.lib.standard_feedback import StandardFeedback
+from app.models.db.oauth2_application import SYSTEM_APP_WEB_CLIENT_ID
 from app.models.db.user import User, UserRole
-from app.models.types import UserId
+from app.models.types import Password, UserId
 from app.queries.audit_query import AuditQuery
 from app.queries.user_query import UserQuery
+from app.services.system_app_service import SystemAppService
+from app.services.user_service import UserService
+from app.validators.display_name import DisplayNameValidating
+from app.validators.email import EmailValidating
 
 router = APIRouter(prefix='/api/web/admin/users')
 
@@ -80,3 +88,45 @@ async def export_ids(
         media_type='application/json',
         headers={'Content-Disposition': 'attachment; filename="user-ids.json"'},
     )
+
+
+@router.post('/{user_id:int}/update')
+async def update_user(
+    _: Annotated[User, web_user('role_administrator')],
+    user_id: UserId,
+    display_name: Annotated[DisplayNameValidating | None, Form()] = None,
+    email: Annotated[EmailValidating | None, Form()] = None,
+    email_verified: Annotated[bool, Form()] = False,
+    roles: Annotated[list[UserRole] | None, Form()] = None,
+    new_password: Annotated[Password | None, Form()] = None,
+):
+    await UserService.admin_update_user(
+        user_id=user_id,
+        display_name=display_name,
+        email=email,
+        email_verified=email_verified,
+        roles=roles or [],
+        new_password=new_password,
+    )
+    return StandardFeedback.success_result(None, 'User has been updated')
+
+
+@router.post('/{user_id:int}/login-as')
+async def login_as_user(
+    _: Annotated[User, web_user('role_administrator')],
+    user_id: UserId,
+):
+    access_token = await SystemAppService.create_access_token(
+        SYSTEM_APP_WEB_CLIENT_ID, user_id=user_id
+    )
+
+    response = RedirectResponse('/', status.HTTP_303_SEE_OTHER)
+    response.set_cookie(
+        key='auth',
+        value=access_token.get_secret_value(),
+        max_age=None,
+        secure=ENV != 'dev',
+        httponly=True,
+        samesite='lax',
+    )
+    return response
