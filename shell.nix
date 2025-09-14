62537e06db45dce5d49d39bc23fcf8867116fcd1
Kamil Monicz <kamil@monicz.dev>
2025-09-14 17:48:07 +0000

    Small reformat shellhook

--- shell.nix
+++ shell.nix
@@ -736,9 +736,10 @@ let
   shell' =
     with pkgs;
     ''
-      cd "${projectDir}"
       [ "$NIX_SSL_CERT_FILE" = "/no-cert-file.crt" ] && unset NIX_SSL_CERT_FILE
       [ "$SSL_CERT_FILE" = "/no-cert-file.crt" ] && unset SSL_CERT_FILE
+
+      cd "${projectDir}"
       export TZ=UTC
       export NIX_ENFORCE_NO_NATIVE=0
       export PROC_COMP_CONFIG=data/pcompose
