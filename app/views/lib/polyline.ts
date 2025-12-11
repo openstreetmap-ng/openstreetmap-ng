9fc9fc282224bda2427ec7510b1afab32f3c3735
Kamil Monicz <kamil@monicz.dev>
2025-12-11 21:23:27 +0000

    Small polyline optimizations

--- app/views/lib/polyline.ts
+++ app/views/lib/polyline.ts
@@ -14,50 +14,57 @@ const ASCII_OFFSET = 63
  */
 const roundPy2 = (value: number) => (value + (value < 0 ? -0.5 : 0.5)) | 0
 
-const encode = (delta: number, codes: number[]) => {
-    let coord = (delta << 1) ^ (delta >> 31) // Zigzag encoding
-    do {
-        const b = coord & CHUNK_MASK
-        coord >>= CHUNK_BITS
-        codes.push(b + (coord ? CONTINUATION_BIT : 0) + ASCII_OFFSET)
-    } while (coord)
-}
-
 export const encodeLonLat = (coords: [number, number][], precision: number) => {
     const factor = 10 ** precision
-    const codes: number[] = []
+    let out = ""
     let prevLatInt = 0
     let prevLonInt = 0
+
+    const encodeDelta = (delta: number) => {
+        let coord = (delta << 1) ^ (delta >> 31) // Zigzag encoding
+        while (coord >= CONTINUATION_BIT) {
+            out += String.fromCharCode(
+                ((coord & CHUNK_MASK) | CONTINUATION_BIT) + ASCII_OFFSET,
+            )
+            coord >>>= CHUNK_BITS
+        }
+        out += String.fromCharCode(coord + ASCII_OFFSET)
+    }
+
     for (const [lon, lat] of coords) {
         const latInt = roundPy2(lat * factor)
         const lonInt = roundPy2(lon * factor)
-        encode(latInt - prevLatInt, codes)
-        encode(lonInt - prevLonInt, codes)
+        encodeDelta(latInt - prevLatInt)
+        encodeDelta(lonInt - prevLonInt)
         prevLatInt = latInt
         prevLonInt = lonInt
     }
-    return String.fromCharCode(...codes)
+    return out
 }
 
 export const decodeLonLat = (line: string, precision: number) => {
-    const invFactor = 10 ** -precision
+    const len = line.length
+    const invFactor = 1 / 10 ** precision
     const coords: [number, number][] = []
     let lat = 0
     let lon = 0
-    for (let i = 0; i < line.length; ) {
-        const decode = () => {
-            let result = 0
-            let shift = 1
-            let code: number
-            do {
-                code = line.charCodeAt(i++) - ASCII_OFFSET
-                result += (code & CHUNK_MASK) * shift
-                shift <<= CHUNK_BITS
-            } while (code >= CONTINUATION_BIT)
-            return (result & 1 ? ~result : result) >> 1
-        }
-        lat += decode()
-        lon += decode()
+    let i = 0
+
+    const decodeDelta = () => {
+        let code: number
+        let result = 0
+        let shift = 0
+        do {
+            code = line.charCodeAt(i++) - ASCII_OFFSET
+            result |= (code & CHUNK_MASK) << shift
+            shift += CHUNK_BITS
+        } while (code >= CONTINUATION_BIT)
+        return (result >> 1) ^ -(result & 1) // Zigzag decode
+    }
+
+    while (i < len) {
+        lat += decodeDelta()
+        lon += decodeDelta()
         coords.push([lon * invFactor, lat * invFactor])
     }
     return coords
