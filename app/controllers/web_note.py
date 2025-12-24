f85bdbe1f266e5b0a96e2168839470cc2cbf1534
Kamil Monicz <kamil@monicz.dev>
2025-12-24 11:46:44 +0000

    Migrate note sidebar to TSX

--- app/controllers/web_note.py
+++ app/controllers/web_note.py
@@ -1,13 +1,16 @@
 from asyncio import TaskGroup
+from math import ceil
 from typing import Annotated, Literal
 
 from fastapi import APIRouter, Form, Query, Response
 from psycopg.sql import SQL
+from shapely import get_coordinates
 from starlette import status
 
 from app.config import (
     NOTE_COMMENT_BODY_MAX_LENGTH,
     NOTE_COMMENTS_PAGE_SIZE,
+    NOTE_FRESHLY_CLOSED_TIMEOUT,
     NOTE_QUERY_AREA_MAX_SIZE,
     NOTE_QUERY_DEFAULT_CLOSED,
     NOTE_QUERY_WEB_LIMIT,
@@ -15,24 +18,28 @@ from app.config import (
 )
 from app.format import FormatRender
 from app.lib.auth_context import web_user
+from app.lib.date_utils import utcnow
 from app.lib.exceptions_context import raise_for
 from app.lib.geo_utils import parse_bbox
 from app.lib.standard_pagination import (
     StandardPaginationStateBody,
     sp_paginate_table,
     sp_render_response,
+    sp_render_response_bytes,
 )
-from app.models.db.note import Note
+from app.models.db.note import Note, note_status
 from app.models.db.note_comment import (
     NoteComment,
     NoteEvent,
     note_comments_resolve_rich_text,
 )
-from app.models.db.user import User
+from app.models.db.user import User, user_avatar_url
+from app.models.proto.shared_pb2 import NoteCommentPage, NoteData
 from app.models.types import Latitude, Longitude, NoteId, UserId
 from app.queries.note_comment_query import NoteCommentQuery
 from app.queries.note_query import NoteQuery
 from app.queries.user_query import UserQuery
+from app.queries.user_subscription_query import UserSubscriptionQuery
 from app.services.note_service import NoteService
 from app.utils import id_response
 
@@ -84,6 +91,58 @@ async def get_map(bbox: Annotated[str, Query()]):
     )
 
 
+@router.get('/{note_id:int}')
+async def get_note(note_id: NoteId):
+    notes = await NoteQuery.find(note_ids=[note_id], limit=1)
+    note = next(iter(notes), None)
+    if note is None:
+        raise_for.note_not_found(note_id)
+
+    async with TaskGroup() as tg:
+        is_subscribed_t = tg.create_task(
+            UserSubscriptionQuery.is_subscribed('note', note_id)
+        )
+        comments = await NoteCommentQuery.resolve_comments(
+            notes, per_note_sort='asc', per_note_limit=1
+        )
+        tg.create_task(UserQuery.resolve_users(comments))
+        tg.create_task(note_comments_resolve_rich_text(comments))
+
+    header = comments[0]
+    header_user = header.get('user')
+    x, y = get_coordinates(note['point'])[0].tolist()
+    status = note_status(note)
+
+    closed_at = note['closed_at']
+    if closed_at is not None:
+        duration = closed_at + NOTE_FRESHLY_CLOSED_TIMEOUT - utcnow()
+        duration_sec = duration.total_seconds()
+        disappear_days = ceil(duration_sec / 86400) if (duration_sec > 0) else None
+    else:
+        disappear_days = None
+
+    result = NoteData(
+        id=note_id,
+        lon=x,
+        lat=y,
+        status=status,
+        header=NoteData.Header(
+            user=NoteData.User(
+                id=header['user_id'],
+                display_name=header_user['display_name'],
+                avatar_url=user_avatar_url(header_user),
+            )
+            if header_user
+            else None,
+            created_at=int(header['created_at'].timestamp()),
+            body_rich=header['body_rich'] if header['body'] else '',  # type: ignore
+        ),
+        is_subscribed=is_subscribed_t.result(),
+        disappear_days=disappear_days,
+    )
+    return Response(result.SerializeToString(), media_type='application/x-protobuf')
+
+
 @router.post('/{note_id:int}/comments')
 async def comments_page(
     note_id: NoteId,
@@ -108,11 +167,26 @@ async def comments_page(
         tg.create_task(UserQuery.resolve_users(comments))
         tg.create_task(note_comments_resolve_rich_text(comments))
 
-    return await sp_render_response(
-        'notes/comments-page',
-        {'comments': comments},
-        state,
+    result = NoteCommentPage(
+        comments=[
+            NoteCommentPage.Comment(
+                user=(
+                    NoteCommentPage.Comment.User(
+                        display_name=comment_user['display_name'],
+                        avatar_url=user_avatar_url(comment_user),
+                    )
+                    if (comment_user := c.get('user')) is not None
+                    else None
+                ),
+                event=c['event'],
+                created_at=int(c['created_at'].timestamp()),
+                body_rich=c.get('body_rich', ''),
+            )
+            for c in comments
+        ]
     )
+
+    return sp_render_response_bytes(result.SerializeToString(), state)
 
 
 @router.post('/user/{user_id:int}')
