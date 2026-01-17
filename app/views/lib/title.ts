1e621c93cae02f1c9307f733ee5a360a8ff333ee
Kamil Monicz <kamil@monicz.dev>
2026-01-17 04:47:47 +0000

    Speed up repeated setPageTitle

--- app/views/lib/title.ts
+++ app/views/lib/title.ts
@@ -1,5 +1,11 @@
+let lastPrefix = ""
+
 const originalTitle = document.title
 
-export const setPageTitle = (prefix?: string) => {
+export const setPageTitle = (prefix?: string | null | undefined) => {
+  prefix ??= ""
+  if (lastPrefix === prefix) return
+  lastPrefix = prefix
+
   document.title = prefix ? `${prefix} | ${originalTitle}` : originalTitle
 }
