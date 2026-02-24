8d70b8ca879dede936f5f79f62979261f32707d4
Kamil Monicz <kamil@monicz.dev>
2026-02-24 21:48:22 +0000

    Use query contract for reports status filter URL

--- app/views/reports/index.ts
+++ app/views/reports/index.ts
@@ -1,13 +1,16 @@
+import { queryParam } from "@lib/codecs"
 import { mount } from "@lib/mount"
-import { qsEncode } from "@lib/qs"
+import { defineQueryContract } from "@lib/query-contract"
 import { configureStandardPagination } from "@lib/standard-pagination"
+
+const FILTER_QUERY = defineQueryContract({ status: queryParam.text() })
 
 mount("reports-body", (body) => {
   const reportStatusFilter = body.querySelector("select#reportStatusFilter")!
   reportStatusFilter.addEventListener("change", () => {
     const status = reportStatusFilter.value
     console.debug("ReportsIndex: Status filter changed", status)
-    window.location.search = qsEncode(status ? { status } : {})
+    window.location.search = FILTER_QUERY.encode({ status })
   })
 
   configureStandardPagination(body.querySelector("div.reports-pagination"))
